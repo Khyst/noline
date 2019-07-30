0df38f855ba5f40f856555553f2aa4edf6567db0
Khyst <kyh9432242@likelion.org>
2019-07-31 00:03:17 +0900

    first

---
+++ nolineApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NolineappConfig(AppConfig):
+    name = 'nolineApp'
---
+++ nolineApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def home(requests):
+    return render(requests, 'nolineApp/index.html')
+
